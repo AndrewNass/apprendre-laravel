fef9b30a504dde6b8ab5566768c564bca2ca6d3f
Andrewnsll <andreif97@gmail.com>
2023-03-28 18:40:00 -0500

    En train de ajouter des Routes.

--- cours-laravel/routes/web.php
+++ cours-laravel/routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(["prefix" => 'administrador'], function() {
+
+    Route::get('/cursos/{cualquiera?}', function($any = 'default ') {
+        return 'Bienvernido '.$any;
+    }) -> where('cualquiera','[A-Za-z0-9.]+');
+});
+
+route::get('/cursos/{cualquiera?}', function($any = 'default ') {
+    return 'hola '.$any;
+});
+
+route::get('/nada', function(){
+    return 'nada';
+});
